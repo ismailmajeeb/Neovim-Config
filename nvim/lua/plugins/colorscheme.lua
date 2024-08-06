5850a1c4ff5d20034445d9d2464c332c464684d3
ismail-mohamed <ismail.majeeb.02@gmail.com>
2024-08-06 20:15:51 +0300

    initial commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,34 @@
+return {
+    --add vim-code-dark Theme
+    { "tomasiser/vim-code-dark" },
+
+    --add Kanagawa Theme
+    { "rebelot/kanagawa.nvim" },
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false, -- do not set background color
+    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    --terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave", -- Load "wave" theme when 'background' option is not set
+    background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+    },
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "kanagawa",
+        },
+    },
+}
